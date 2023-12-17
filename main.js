10af7cbc855e67d9e6a24bd3500f17dcd2cd13e5
stobiewan <getsforwarded@gmail.com>
2023-12-17 15:00:14 +1300

    start adding uni ilk set for sepolia deploy

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-import { createPublicClient, createWalletClient, custom, formatUnits, http, parseAbi, stringToHex, toHex } from 'viem'
-import { arbitrumGoerli } from 'viem/chains'
+import { createPublicClient, createWalletClient, custom, formatUnits, getContract,
+    hexToString, http, pad, parseAbi, parseUnits, stringToHex, toHex } from 'viem'  // todo update to viem 2.0
+import { sepolia } from 'viem/chains'
 
-const fbAddress  = "0x1dE162F7B87A9290c639f6f7bd4b3ea9a7B1a355"
-const bankAddress = "0x5D00C6A7aB8614Ebc5d8E9bD79089526B1469024"
+// sepolia addresses
+const bankAddress   = "0x343d30cCCe6c02987329C4fE2664E20F0aD39aa2"
+const feedAddress   = "0x16Bb244cd38C2B5EeF3E5a1d5F7B6CC56d52AeF3"
+const nfpmAddress   = "0x1238536071E1c677A632429e3655c799b22cDA52"
+const rico_addr     = "0x6c9BFDfBbAd23418b5c19e4c7aF2f926ffAbaDfa"
+const dai_addr      = "0x290eCE67DDA5eEc618b3Bb5DF04BE96f38894e29"
 
-const fbAbi  = parseAbi([
+const arb_addr      = "0x3c6765dd58D75786CD2B20968Aa13beF2a1D85B8"
+const stable_addr   = "0x698DEE4d8b5B9cbD435705ca523095230340D875"
+const wdiveth_addr  = "0x69619b71b52826B93205299e33259E1547ff3331"
+
+const ricorisk_addr = "0x5dD4Ff6070629F879353d02fFdA3404085298669"
+const ricodai_addr  = "0x6443Da3Df6DAE6F33e53611f31ec90d101Bf7FbF"
+
+const gems = {
+    arb:     arb_addr,
+    wdiveth: wdiveth_addr,
+    stable:  stable_addr
+}
+
+const uniIlk = stringToHex(":uninft", {size: 32})
+
+const feedAbi  = parseAbi([
     "function pull(address src, bytes32 tag) external view returns (bytes32 val, uint256 ttl)"
 ])
 const gemAbi = parseAbi([
@@ -12,23 +32,34 @@ const gemAbi = parseAbi([
     "function approve(address usr, uint256 wad) external payable returns (bool ok)",
     "function balanceOf(address) external view returns (uint256)"
 ])
+const nfpmAbi = parseAbi([
+    "function approve(address to, uint256 tokenId)",
+    "function getApproved(uint256 tokenId) returns (address operator)",
+    "function setApprovalForAll(address operator, bool approved)",
+    "function isApprovedForAll(address owner, address operator) returns (bool approved)",
+    "function balanceOf(address owner) view returns (uint)",
+    "function positions(uint256 tokenId) view returns (uint96 nonce, address operator, address token0, address token1, uint24 fee, int24 tickLower, int24 tickUpper, uint128 liquidity, uint256 feeGrowthInside0LastX128, uint256 feeGrowthInside1LastX128, uint128 tokensOwed0, uint128 tokensOwed1)",
+    "function tokenOfOwnerByIndex(address owner, uint256 index) view returns (uint256)"
+])
+
+// todo mainly unused, should use something like this to avoid multiple roundtrips reading state to use to read state
+const tokenData = {
+    "arb":     { src: "0x111", tag: "tag", decimals: 18 },
+    "wdiveth": { src: "0x111", tag: "tag", decimals: 18 },
+    "stable":  { src: "0x111", tag: "tag", decimals: 18 },
+};
 
 import BankDiamond from './BankDiamond.json';
 const bankAbi = BankDiamond.abi
 
-const gems = {weth: "0xa9d267C3334fF4F74836DCbFAfB358d9fDf1E470", reth: "0x0", gold: "0x0"}
-
-const bankConfig = {
-    address: bankAddress,
-    abi: bankAbi
-};
 const fbConfig = {
-    address: fbAddress,
-    abi: fbAbi
+    address: feedAddress,
+    abi: feedAbi
 }
 
-let account, clientPub, clientWal
+let account, publicClient, walletClient
 let usrGemBal, usrGemAllowance
+let bank, nfpm, feed
 
 const $ = document.querySelector.bind(document);
 const BANKYEAR = ((365 * 24) + 6) * 3600
@@ -38,42 +69,106 @@ const round =f=> parseFloat(f).toPrecision(4)
 
 const updateRicoStats = async () => {
     $('#ricoStats').textContent = ' '
-    const parRay = await clientPub.readContract({...bankConfig, functionName: 'par'})
+
+    const [parRay, wayRay, tipData] = await Promise.all([
+        bank.read.par(),
+        bank.read.way(),
+        bank.read.tip(),
+    ]);
+    const { src, tag } = tipData;
+
     const par = formatUnits(parRay, 27)
-    const wayRay = await clientPub.readContract({...bankConfig, functionName: 'way'})
     const way = apy(wayRay)
-    const {src, tag} = await clientPub.readContract({...bankConfig, functionName: 'tip'})
     let mar
     try {
-        const [val, ttl] = await clientPub.readContract({...fbConfig, functionName: 'pull', args: [src, tag]})
+        const [val, ttl] = await publicClient.readContract({...fbConfig, functionName: 'pull', args: [src, tag]})
         mar = formatUnits(BigInt(val), 27)
     } catch (e) {
         console.log(`failed to read market price of rico with src ${src} and tag ${tag}`)
         console.log(e)
         mar = -1.0
     }
-    $('#ricoStats').textContent = `Rico system price: ${round(par)}, market price: ${round(mar)}, Price rate: ${way}%`
+    $('#ricoStats').textContent = `Rico system price: ${round(par)}, Price rate: ${way}%, market price: ${round(mar)}`
 }
 
-const updateIlkStats = async () => {
+const updateHook = async () => {
+    const showUni = $('input[name="ctype"]:checked').value == "UNIV3 LP NFTs"
+    document.getElementById("uniHook").style.display   = showUni ? "flex" : "none";
+    document.getElementById("erc20Hook").style.display = showUni ? "none"  : "block";
+    if(showUni){
+        await updateUni()
+    } else {
+        await updateERC20IlkStats()
+        await updateERC20UrnStats()
+    }
+}
+
+const updateUni = async () => {
+    const container = document.getElementById('nftContainer');
+    container.innerHTML = '';
+    // either get deposited ink, or users own available NFTs
+    if ($('input[name="sign"]:checked').value == "borrow") {
+        document.getElementById("NFTList").textContent = `LP NFTS to deposit:`;
+        const numNFTs = await nfpm.read.balanceOf([account])
+        const idsProm = Array.from({ length: Number(numNFTs) }, (_, i) => nfpm.read.tokenOfOwnerByIndex([account, i]));
+        const ids = await Promise.all(idsProm);
+        displayNfts(ids)
+    } else {
+        // get ink from bank
+        document.getElementById("NFTList").textContent = `LP NFTS to withdraw:`;
+        const uniInk = await bank.read.ink([uniIlk, account])
+        console.log(uniInk)
+    }
+}
+
+// todo this displays checkboxes for each uni nft with token IDs for labels
+// symbol/symbol min price, max price could be better. People can find id in uni app
+function displayNfts(nftIds) {
+    const container = document.getElementById('nftContainer');
+
+    nftIds.forEach(id => {
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = `nft-${id}`;
+        checkbox.value = id;
+
+        const label = document.createElement('label');
+        label.htmlFor = `nft-${id}`;
+        label.textContent = `NFT ID: ${id}`;
+
+        const div = document.createElement('div');
+        div.style.marginRight = '10px'
+        div.appendChild(checkbox);
+        div.appendChild(label);
+
+        container.appendChild(div);
+    });
+}
+
+function getSelectedNfts() {
+    const checkboxes = document.querySelectorAll('#nftContainer input[type="checkbox"]:checked');
+    return Array.from(checkboxes).map(checkbox => BigInt(checkbox.value));
+}
+
+const updateERC20IlkStats = async () => {
     $('#ilkStats').textContent = ' '
     const ilkStr = $('input[name="ilk"]:checked').value
     const ilkHex = stringToHex(ilkStr, {size: 32})
     $('#gem').textContent = ilkStr
-    const ilk  = await clientPub.readContract({...bankConfig,
-                                            functionName: 'ilks',
-                                            args: [ilkHex]})
+    const ilk  = await bank.read.ilks([ilkHex])
     const fee  = apy(ilk.fee)
     const dust = formatUnits(ilk.dust, 45)
-    $('#ilkStats').textContent = `Quantity rate: ${fee}%, Min debt: ${round(dust)}`
+    $('#ilkStats').textContent = `Quantity rate: ${fee}%, Min debt: ${round(dust)} rico`
 
-    usrGemAllowance = await clientPub.readContract({
+    // todo 3 serial waits, replace with single viem multicall "await publicClient.multicall..."
+    // or at least concurrent promise group
+    usrGemAllowance = await publicClient.readContract({
         address: gems[ilkStr],
         abi: gemAbi,
         functionName: 'allowance',
         args: [account, bankAddress]
     })
-    usrGemBal = await clientPub.readContract({
+    usrGemBal = await publicClient.readContract({
         address: gems[ilkStr],
         abi: gemAbi,
         functionName: 'balanceOf',
@@ -84,85 +179,147 @@ const updateIlkStats = async () => {
 const getHookStats = async () => {
     const ilkStr = $('input[name="ilk"]:checked').value
     const ilkHex = stringToHex(ilkStr, {size: 32})
-    const ink = await clientPub.readContract({...bankConfig,
-                                           functionName: 'ink',
-                                           args: [ilkHex, account]})
-    const rawSrc = await clientPub.readContract({...bankConfig,
-                                              functionName: 'geth',
-                                              args: [ilkHex, stringToHex('src', {size: 32}), []]})
-    const tag    = await clientPub.readContract({...bankConfig,
-                                              functionName: 'geth',
-                                              args: [ilkHex, stringToHex('tag', {size: 32}), []]})
-    const src = rawSrc.slice(0, 42);
 
+    const [ink, rawSrc, tag] = await Promise.all([
+      bank.read.ink( [ilkHex, account]),
+      bank.read.geth([ilkHex, stringToHex('src', {size: 32}), []]),
+      bank.read.geth([ilkHex, stringToHex('tag', {size: 32}), []]),
+    ])
+    const inkStr = formatUnits(BigInt(ink), tokenData[ilkStr].decimals)
+    // todo use tokenData{} to get src, tag and reduce to one fetch
+    const src = rawSrc.slice(0, 42);
     let mark;
     try {
-        const [val, ttl] = await clientPub.readContract({...fbConfig, functionName: 'pull', args: [src, tag]})
+        const [val, ttl] = await publicClient.readContract({...fbConfig, functionName: 'pull', args: [src, tag]})
         mark = formatUnits(BigInt(val), 27)
     } catch(e) {
         console.log(e)
         mark = -1.0
     }
-    return `Feed price: ${round(mark)}, ${ilkStr} collateral: ${round(ink)}`
+    return `Feed price: ${round(mark)}, ${ilkStr} collateral: ${round(inkStr)}`
 }
 
-const updateUrnStats = async () => {
+const updateERC20UrnStats = async () => {
     const ilkStr = $('input[name="ilk"]:checked').value
     const ilkHex = stringToHex(ilkStr, {size: 32})
-    const ilk  = await clientPub.readContract({...bankConfig,
-                                            functionName: 'ilks',
-                                            args: [ilkHex]})
-    const urn  = await clientPub.readContract({...bankConfig,
-                                            functionName: 'urns',
-                                            args: [ilkHex, account]})
+
+    const [ilk, urn] = await Promise.all([
+      bank.read.ilks([ilkHex]),
+      bank.read.urns([ilkHex, account]),
+    ])
+
     const debt = formatUnits(urn * ilk.rack, 45)
     const hookStats = await getHookStats()
     $('#urnStats').textContent = `rico debt: ${round(debt)}, ${hookStats}`
 }
 
+const frobUni = async () => {
+    const sign = ($('input[name="sign"]:checked').value == "repay") ? "-" : ""
+    let dink = "0x"
+    const dart = parseUnits(sign + $('#uniDart').value, 18)
+    const nfts = getSelectedNfts()
+
+    if (nfts.length > 0) {
+        dink = sign == "-" ? pad(toHex(-1)) : pad(toHex(1))
+
+        if (!await nfpm.read.isApprovedForAll([account, bankAddress])) {
+            await nfpm.write.setApprovalForAll([bankAddress, true])
+        }
+        for (let id of nfts) {
+            dink += pad(toHex(id)).slice(2)
+        }
+    }
+
+    // todo deposit frob with NFTs always reverting on sepolia. config?
+
+    // const { request } = await bank.simulate.frob([uniIlk, account, dink, dart], {gasLimit:10_000_000, from: account})
+    // await walletClient.writeContract(request)
+    await bank.write.frob([uniIlk, account, dink, dart], {gasLimit:6_000_000})
+
+    await updateHook()
+}
+
+const frobERC20 = async () => {
+    const ilk = $('input[name="ilk"]:checked').value
+    const sign = ($('input[name="sign"]:checked').value == "repay") ? "-" : ""
+    let dink = parseUnits(sign + $('#dink').value, tokenData[ilk].decimals);
+    const dart = parseUnits(sign + $('#dart').value, 18)
+    if (dink > usrGemAllowance) {
+        const {request} = await publicClient.simulateContract({
+            abi: gemAbi,
+            address: gems[ilk],
+            functionName: 'approve',
+            args: [bankAddress, MAXUINT],
+            account: account,
+        })
+        await walletClient.writeContract(request)
+    }
+
+    if (dink < 0) dink += MAXUINT
+    const dinkB32 = pad(toHex(dink))
+
+    await bank.write.frob([stringToHex(ilk, {size: 32}), account, dinkB32, dart])
+    await updateERC20UrnStats()
+}
+
 window.onload = async() => {
     [account] = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    clientWal = createWalletClient({
+    walletClient = createWalletClient({
       account,
-      chain: arbitrumGoerli,
+      chain: sepolia,
       transport: custom(window.ethereum)
     })
-    clientPub = createPublicClient({
+    publicClient = createPublicClient({
       batch: {
         multicall: true,
       },
-      chain: arbitrumGoerli,
+      chain: sepolia,
       transport: http(),
-})
+    })
 
-    $('#btnFrob').addEventListener('click', async () =>  {
-        const ilk = $('input[name="ilk"]:checked').value
-        const sign = ($('input[name="sign"]:checked').value == "repay") ? "-" : ""
-        const dink = parseUnits(sign + $('#dink').value, 18);
-        const dart = parseUnits(sign + $('#dart').value, 18)
-        if (dink > usrGemAllowance) {
-            const { request } = await publicClient.simulateContract({
-              abi: gemAbi,
-              address: gems[ilk],
-              functionName: 'approve',
-              args: [MAXUINT],
-              account: account,
-            })
-            await walletClient.writeContract(request)
+    bank = getContract({
+      address: bankAddress,
+      abi: bankAbi,
+      publicClient: publicClient,
+      walletClient: walletClient
+    })
+    feed = getContract({
+      address: feedAddress,
+      abi: feedAbi,
+      publicClient: publicClient,
+      walletClient: walletClient
+    })
+    nfpm = getContract({
+      address: nfpmAddress,
+      abi: nfpmAbi,
+      publicClient: publicClient,
+      walletClient: walletClient
+    })
+
+    $('#btnFrob').addEventListener('click', async () => {
+        if ($('input[name="ctype"]:checked').value == "UNIV3 LP NFTs") {
+            await frobUni()
+        } else {
+            await frobERC20()
         }
-        const urn = account
-        const dinkB32 = pad(toHex(dink))
-        await bank.frob(stringToHex(ilk, {size: 32}), urn, dinkB32, dart, {gasLimit:10000000})
-        await updateUrnStats()
     });
 
-    document.querySelectorAll('input[name="ilk"]').forEach((elem) => {
+    document.querySelectorAll('input[name="ctype"]').forEach((elem) => {
         elem.addEventListener("change", async () => {
-            await updateIlkStats()
+            await updateHook()
         });
     });
+    document.querySelectorAll('input[name="sign"]').forEach((elem) => {
+        elem.addEventListener("change", async () => {
+            await updateHook()
+        });
+    });
+    document.querySelectorAll('input[name="ilk"]').forEach((elem) => {
+        elem.addEventListener("change", async () => {
+            await updateERC20IlkStats()
+        });
+    });
+    // todo update on palms
 
-    await updateRicoStats()
-    await updateIlkStats()
-    await updateUrnStats()
+    await Promise.all([updateRicoStats(), updateHook()]);
 }
