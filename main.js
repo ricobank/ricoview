dba4bec15fb5fcc8928e6ecd579890431060d3a5
kbrav <3114369+kbrav@users.noreply.github.com>
2024-02-01 15:33:07 -0500

    par 7 sig fig (#29)

--- main.js
+++ main.js
@@ -100,7 +100,7 @@ const updateRicoStats = async () => {
     const par = formatUnits(parRay, 27)
     const way = apy(wayRay)
     const mar = formatUnits(BigInt(feedData[0]), 27)
-    ricoStats.textContent = `Par: ${round(par)}, Price rate: ${way}%, Market: ${round(mar)}`
+    ricoStats.textContent = `Par: ${round7(par)}, Price rate: ${way}%, Market: ${round(mar)}`
 }
 
 const updateHook = async () => {
@@ -561,6 +561,7 @@ const maxBigInt = (a, b) => a > b ? a : b
 const apy =r=> round(((Number(r) / 10**27) ** BANKYEAR - 1) * 100)
 
 const round =f=> parseFloat(f).toPrecision(4)
+const round7 =f=> parseFloat(f).toPrecision(7)
 
 const x20 = (s) => stringToHex(s, {size: 20})
 
